5e9466d10b72f2acffc14cc2839b38c5bba1729b
CrazyStitch <stitch_crazy@163.com>
2022-11-26 10:45:29 +0800

    提交

--- Autosshcmd/autossh.py
+++ Autosshcmd/autossh.py
@@ -26,8 +26,10 @@ def ssh_connect(node_ip, username, passwd, command):
 
 
 # 解析json格式信息
-def read_json():
+def read_json(json_file):
+    json.load(json_file)
     pass
+
 
 # 解析自定义格式信息
 def read_cfg(cfgfile):
@@ -40,28 +42,27 @@ def read_cfg(cfgfile):
                 try:
                     pass
                 except Exception as e:
+                    print("未知错误，请查看：%s" % e)
                     continue
                 # 开始执行脚本
                 pass
+
 
 # 拼接命令(执行自动挂盘并格式化目录)
 def mount_disk():
 
     mount = "fdisk -l;dmsetup remove_all;yes n|mkfs -t ext3 /dev/vdb;mount /dev/vdb /opt"
     # 修改文件系统静态信息文件
-    editfile = """"/dev/vdb  /opt  ext3  defaults  0  0" >> /etc/fstab;df -h"""
+    edit_file = """"/dev/vdb  /opt  ext3  defaults  0  0" >> /etc/fstab;df -h"""
 
     # 拼接需要执行的命令
-    lastcmd = mount + ";" + editfile
-    return command
+    last_cmd = mount + ";" + edit_file
+    return last_cmd
+
 
 def execute_cmd(hostip, password, sshcmd):
-
-        res, err  = ssh_connect(hostip, "root", password, sshcmd)
-
-
-
-
+    res, err = ssh_connect(hostip, "root", password, sshcmd)
+    return res, err
 
 
 if __name__ == "__main__":
